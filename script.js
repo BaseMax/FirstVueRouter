8c752fe31acdea9c4713998d6a7748358cc1ce87
Max Base <MaxBaseCode@Gmail.Com>
2020-07-28 14:04:06 +0430

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,49 @@
  * @Repository: https://github.com/BaseMax/FirstVueRouter/new/master
  * @Date: 2020-07-27
 */
+const HomePage = {
+  template: '#home-page',
+  data: function() {
+    return {
+      lang:this.$route.params.lang ? this.$route.params.lang : 'en',
+    };
+  },
+}
+
+const CategoryPage = {
+  template: '#category-page',
+}
+
+const SearchPage = {
+  template: '#search-page',
+}
+
+const ApplicationPage = {
+  template: '#application-page',
+}
+
+const NotFoundComponent = {
+  template: '#error-page',
+}
+
+const routes = [
+  { path: '/:lang?', component: HomePage },
+  { path: '/:lang/search', component: SearchPage },
+  { path: '/:lang/:cat', component: CategoryPage },
+  { path: '/:lang/:cat/:app', component: ApplicationPage },
+  { path: '/:lang/*', component: NotFoundComponent },
+  { path: '*', component: NotFoundComponent }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes,
+})
+
+const app = new Vue({
+  el: '#app',
+  router: router,
+  mounted: function() {
+    // console.log(this.$router.currentRoute);
+  },
+});
