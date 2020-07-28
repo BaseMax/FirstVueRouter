7354f4fd7e5fa32fb88e44288d06ffbdff420c24
Max Base <MaxBaseCode@Gmail.Com>
2020-07-28 14:05:37 +0430

    Update serve.js

--- serve.js
+++ serve.js
@@ -1,43 +1,13 @@
-const http = require('http')
-const fs = require('fs')
-const httpPort = 8181
+const express = require('express');
+const app = express();
 
-http.createServer((req, res) => {
-  let file='index.html';
-  console.log(req.url);
-  if(req.url.startsWith("/font/") || req.url == '/style.css') {
-    file=req.url.substring(1);
-  }
-  fs.readFile(file, 'utf-8', (err, content) => {
-    if (err) {
-      console.log('We cannot open "index.html" file.')
-    }
-    if(file !== 'index.html') {
-      let format=req.url.split('.');
-      // console.log(format);
-      // console.log(format[format.length-1]);
-      format=format[format.length-1];
-      let mime='';
-      if(format == 'woff') {
-        mime='application/font-woff';
-      }
-      else if(format == 'woff2') {
-        mime='font/woff2';
-      }
-      else if(format == 'css') {
-        mime='text/css';
-      }
-      res.writeHead(200, {
-        'Content-Type': mime+';'
-      })
-    }
-    else {
-      res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8'
-      })
-    }
-    res.end(content)
-  })
-}).listen(httpPort, () => {
-  console.log('Server listening on: http://localhost:%s', httpPort)
-})
+app.use("/asset", express.static(__dirname + "/asset"));
+app.use(function(req, res, next) {
+    // res.status(404).send('Unable to find the requested resource!');
+    res.sendFile('/home/max/Desktop/vuejs/vuejs.html');
+});
+// app.use(express.static('.'));
+// app.get('*', (req, res) => {
+//   res.sendFile('index.html');
+// });
+app.listen(8282, () => console.log('Gator app listening on port 8282!'));
