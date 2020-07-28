c9254f38d5452cf651d090932a7673e093100d74
Max Base <MaxBaseCode@Gmail.Com>
2020-07-28 14:06:10 +0430

    Update serve.js

--- serve.js
+++ serve.js
@@ -1,13 +1,14 @@
+/*
+ * @Name: First Vue-Router
+ * @Author: Max Base
+ * @Repository: https://github.com/BaseMax/FirstVueRouter/new/master
+ * @Date: 2020-07-27
+*/
 const express = require('express');
 const app = express();
 
 app.use("/asset", express.static(__dirname + "/asset"));
 app.use(function(req, res, next) {
-    // res.status(404).send('Unable to find the requested resource!');
     res.sendFile('/home/max/Desktop/vuejs/vuejs.html');
 });
-// app.use(express.static('.'));
-// app.get('*', (req, res) => {
-//   res.sendFile('index.html');
-// });
-app.listen(8282, () => console.log('Gator app listening on port 8282!'));
+app.listen(8282, () => console.log('App listening on port 8282!'));
